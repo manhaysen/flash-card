d21bb12e4ca1d8a74039a29434f607e73217d239
Mariia <pravdova97@mail.ru>
2023-01-20 16:50:57 +0300

    finish

--- views/Result.jsx
+++ views/Result.jsx
@@ -4,11 +4,18 @@ const Layout = require('./Layout');
 module.exports = function Result({ title, user }) {
   return (
     <Layout title={title} user={user}>
-      <div className='result_score'>
-
-        {/* <h1>Ваш результат</h1> */}
-        <div>{user.name}, поздравляем! Ваш результат:</div>
-        <div>{user.score} баллов</div>
+      <div className="container">
+        <div className="result_score">
+          {/* <h1>Ваш результат</h1> */}
+          <div>{user.name}, поздравляем! Ваш результат:</div>
+          <div>{user.score} баллов</div>
+          {user.score < 30 ? (
+            <p>Семья отвергла вас</p>
+          ) : (
+            <p>Вы приняты в семью</p>
+          )}
+        </div>
+        <img src="/img/imgLike.png" alt="likee" className="game__like" />
       </div>
     </Layout>
   );
